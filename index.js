afea742967b6428d198690e680ad2f5e54f6d687
Lauri <lapakka@gmail.com>
2017-03-21 13:48:17 +0200

    Universal CORS allow added and minor text fixes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.Promise = global.Promise;
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
